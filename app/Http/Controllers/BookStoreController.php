278faca50760c8e7bfaf1e7c8780ef90c7a80774
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2020-09-24 03:21:37 -0700

    Daniel: Generated Migrations, seeder, controller, factory, model for Advertisement, bookstore, usermedia and bookstore

---
+++ app/Http/Controllers/BookStoreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book_Store;
+use Illuminate\Http\Request;
+
+class BookStoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book_Store  $book_Store
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book_Store $book_Store)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book_Store  $book_Store
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book_Store $book_Store)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book_Store  $book_Store
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book_Store $book_Store)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book_Store  $book_Store
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book_Store $book_Store)
+    {
+        //
+    }
+}
