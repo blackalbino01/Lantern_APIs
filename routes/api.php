bb8d3bbcc1ba696d0133c05f4d7a9f0b9c4c5df0
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2020-10-02 12:12:35 -0700

    hmmm

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\UserMediaController;
 use App\Http\Controllers\BookStoreController;
-=======
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\User_ProfileController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SubjectController;
-use App\Http\Controllers\SkillController;
-use App\Http\Controllers\InterestController;
-
->>>>>>> a7bb5b7d277852773c04dda01742fe1be0425f06
 
 /*
 |--------------------------------------------------------------------------
@@ -30,18 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
-// All User media related api route
-/*
-    you can view all endpoints using the <php artisan route:list> artisan command
-*/
-
-Route::apiResource('media', UserMediaController::class);
-// ->middleware('auth:api');
-Route::apiResource('books', BookStoreController::class);
-
-// Route::get('user_media', [UserMediaController::class, 'index']);
-=======
 Route::post('category/{category}/skill',[SkillController::class,'store']);
 Route::post('category/{category}/subject',[SubjectController::class,'store']);
 Route::post('category/{category}/interest',[InterestController::class,'store']);
@@ -71,4 +49,6 @@ Route::group([
 	Route::patch('userprofile/{id}',[User_ProfileController::class,'update']);
 	Route::delete('userprofile/{id}',[User_ProfileController::class,'destroy']);
 });
->>>>>>> a7bb5b7d277852773c04dda01742fe1be0425f06
+
+Route::apiResources('books', BookStoreController::class);
+Route::apiResources('media', UserMediaController::class);
